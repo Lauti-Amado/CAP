4aa4c32117a9a5f6e15a2d9fd54a4d60aaed9359
juan-caceres <jcruz2013c@gmail.com>
2026-09-21 16:00:17 -0300

    fix: Cambia imports de componente

--- src/Pantallas/seleccionarCarrera/seleccionarCarrera.tsx
+++ src/Pantallas/seleccionarCarrera/seleccionarCarrera.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Sidebar from '../../componentes/Sidebar';
-import './seleccionarCarrera.css';
+import './SeleccionarCarrera.css';
 
 const SeleccionarCarrera = () => {
 
